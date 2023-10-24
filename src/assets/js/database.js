499cd27495d471b5aa64faee3bcbce96b88c4fed
LuanGDantas <luan.dantas123@hotmail.com>
2023-10-24 20:38:32 -0300

    Database em memória

---
+++ src/assets/js/database.js
@@ -0,0 +1,54 @@
+const database = {
+    areas: [
+        {
+            nome: 'DevOps',
+            descricao: 'É a área responsável por aplicar um conjunto de práticas e processos que visam acelerar e automatizar aspectos dos processos de desenvolvimento, testes e lançamento de softwares.'
+        },
+        {
+            nome: 'Ánalise de dados',
+            descricao: 'É a área que transforma números e informações em insights para a tomada de decisão, utilizando de técnicas estatísticas e lógicas.'
+        },
+        {
+            nome: 'Segurança da Informação',
+            descricao: 'É a área da informática que tem a missão de proteger todas as informações e dados que circulam na rede.'
+        }
+    ],
+    curso: [
+        {
+            nome: 'DevOps & Agile Culture',
+            nivel: 'Intermediário',
+            cargaHoraria: '60h'
+        }
+    ],
+    vagas: [
+        {
+            nome: 'Back-End',
+            nivel: 'Pleno',
+            salario: 'A combinar',
+            local: 'Rio de Janeiro (Híbrido)'
+        },
+        {
+            nome: 'Front-End',
+            nivel: 'Pleno',
+            salario: 'A combinar',
+            local: 'São Paulo'
+        }
+    ],
+    membros: [
+        {
+            nome: '',
+            descricao: '',
+            github: '',
+            linkedin: ''
+        }
+    ],
+    feedbacks: [
+        {
+            nome: 'João de Deus',
+            email: 'joao@gmail.com',
+            telefone: '84999551234',
+            site: '',
+            mensagem: 'Site muito bom!'
+        }
+    ],
+}
