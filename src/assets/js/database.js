efda821b2eb534e74128b274909d8e24d925c4a2
LuanGDantas <luan.dantas123@hotmail.com>
2023-10-31 19:31:36 -0300

    Adicionado campo de imagem

--- src/assets/js/database.js
+++ src/assets/js/database.js
@@ -2,22 +2,26 @@ const database = {
     areas: [
         {
             nome: 'DevOps',
-            descricao: 'É a área responsável por aplicar um conjunto de práticas e processos que visam acelerar e automatizar aspectos dos processos de desenvolvimento, testes e lançamento de softwares.'
+            descricao: 'É a área responsável por aplicar um conjunto de práticas e processos que visam acelerar e automatizar aspectos dos processos de desenvolvimento, testes e lançamento de softwares.',
+            imagem: ''
         },
         {
             nome: 'Ánalise de dados',
-            descricao: 'É a área que transforma números e informações em insights para a tomada de decisão, utilizando de técnicas estatísticas e lógicas.'
+            descricao: 'É a área que transforma números e informações em insights para a tomada de decisão, utilizando de técnicas estatísticas e lógicas.',
+            imagem: ''
         },
         {
             nome: 'Segurança da Informação',
-            descricao: 'É a área da informática que tem a missão de proteger todas as informações e dados que circulam na rede.'
+            descricao: 'É a área da informática que tem a missão de proteger todas as informações e dados que circulam na rede.',
+            imagem: ''
         }
     ],
     curso: [
         {
             nome: 'DevOps & Agile Culture',
             nivel: 'Intermediário',
-            cargaHoraria: '60h'
+            cargaHoraria: '60h',
+            imagem: ''
         }
     ],
     vagas: [
@@ -25,13 +29,15 @@ const database = {
             nome: 'Back-End',
             nivel: 'Pleno',
             salario: 'A combinar',
-            local: 'Rio de Janeiro (Híbrido)'
+            local: 'Rio de Janeiro (Híbrido)',
+            imagem: ''
         },
         {
             nome: 'Front-End',
             nivel: 'Pleno',
             salario: 'A combinar',
-            local: 'São Paulo'
+            local: 'São Paulo',
+            imagem: ''
         }
     ],
     membros: [
@@ -39,7 +45,8 @@ const database = {
             nome: '',
             descricao: '',
             github: '',
-            linkedin: ''
+            linkedin: '',
+            foto: '' 
         }
     ],
     feedbacks: [
