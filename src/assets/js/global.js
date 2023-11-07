830ddf8685373bc1cdbfe2a3509a45c2a101d554
LuanGDantas <luan.dantas123@hotmail.com>
2023-11-07 19:12:14 -0300

    Adicionado evento de click ao botão de menu

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,15 +1,18 @@
-new window.VLibras.Widget('https://vlibras.gov.br/app');
-
 function mostarMenuFluante() {
-    let header = document.querySelector('header');
-    let menu = document.querySelector('.menu');
-    let buttonMenu =  document.querySelector('.button-menu');
-    menu.classList.toggle('expanded-menu');
-    if(menu.classList.contains('expanded-menu')) {
-      header.style.background = 'linear-gradient(145deg, #2D266C 0%, #375ee3 80%)';
-      buttonMenu.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    } else {
-      header.style.removeProperty('background');
-      buttonMenu.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  let header = document.querySelector('header');
+  let menu = document.querySelector('.menu');
+  let buttonMenu =  document.querySelector('.button-menu');
+  menu.classList.toggle('expanded-menu');
+  if(menu.classList.contains('expanded-menu')) {
+    header.style.background = 'linear-gradient(145deg, #2D266C 0%, #375ee3 80%)';
+    buttonMenu.innerHTML = '<i class="material-symbols-rounded">close</i>';
+  } else {
+    header.style.removeProperty('background');
+      buttonMenu.innerHTML = '<i class="material-symbols-rounded">menu</i>';
     }
 } 
+
+const buttonMenu = document.querySelector('.button-menu');
+buttonMenu.addEventListener('click', mostarMenuFluante);
+
+new window.VLibras.Widget('https://vlibras.gov.br/app');
