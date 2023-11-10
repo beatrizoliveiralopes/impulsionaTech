b4a7072a0cc1d2b8c0114eb7f4c67c957b1f6573
sabrinaflorenco <sabrinaflorenco@bol.com.br>
2023-11-09 22:36:24 -0300

    adicionado dados cards

--- src/assets/js/database.js
+++ src/assets/js/database.js
@@ -48,9 +48,36 @@ const database = {
             linkedin: 'https://br.linkedin.com/in/beatriz-oliveira-lopes-0930b562',
             foto: '../assets/imgs/beatriz.jpg' 
         },
+        { 
+            nome:'Deyvison',
+            descricao:'Formado em Análise e Desenvolvimento de Sistemas pela Universidade da Amazônia, desde 2015.Possui experiência em desenvolvimento Front-End. Trabalhou em instituições públicas e privadas.',
+            github:'https://github.com/DeyvisonViana',
+            linkedin:'https://www.linkedin.com/in/deyvison-viana-paes-71236a5a',
+            foto:'../assets/imgs/deyvison.jpg'
+        },
         {
-            
-        }
+            nome:'Leandro',
+            descricao:'Cursando Análise e Desenvolvimento de Sistemas na Uniasselvi. Em processo de transição de carreira, saindo do comércio para ingressar no mundo da tecnologia.',
+            github:'https://github.com/LeandroGuimaraes23',
+            linkedin:'https://www.linkedin.com/in/leandro-guimar%C3%A3es-aa7115249?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+            foto:'../assets/imgs/leandro.jpg',
+
+        },
+        {
+            nome:'Luan',
+            descricao:'Graduado em Bacharelado em TI e em Engenharia de Computação, ambas pela Universidade Federal Rural do Semi-Árido/UFERSA. Tenho experiência em back-end (API REST) com Node.js, graças a um projeto de iniciação científica e um estágio.',
+            github:'https://github.com/LuanGDantas',
+            linkedin:'https://www.linkedin.com/in/luan-guilherme-dantas-3a57a3130/?originalSubdomain=br',
+            foto:'../assets/imgs/LUAN.jpg',
+        },
+      
+        {
+            nome:'Sabrina',
+            descricao:'Estou em transição de carreira minha atuação no mercado de trabalho era Inspetor de Controle de Qualidade. Atualmente cursando Análise e Desenvolvimento de Dados.',
+            github:'https://github.com/sabrinaflorenco',
+            linkedin:'https://www.linkedin.com/in/sabrina-dias-inspetoradequalidade/',
+            foto:'../assets/imgs/sabrina.jpg',
+        },
     ],
     feedbacks: [
         {
