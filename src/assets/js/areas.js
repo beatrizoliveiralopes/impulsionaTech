f412a1ccb2b2632622b1e658df3df495939e1a52
beatrizoliveiralopes <131905611+beatrizoliveiralopes@users.noreply.github.com>
2023-11-10 17:22:34 -0300

    páginas cursos e áreas dinamicas

---
+++ src/assets/js/areas.js
@@ -0,0 +1,26 @@
+
+const {areas} = database;
+const conteiner = document.querySelector('.card__container');
+conteiner.innerHTML = '';
+
+for (let {imagem, titulo, descricao} of areas)
+{
+  const areas = document.createElement('div'); 
+  areas.classList.add('card__bx');
+  areas.innerHTML = 
+        ` <div class="card__data">
+              <div class="card__icon">
+                <img src="${imagem}" width="83px" height="80px"/>
+              </div>
+              <div class="card__content">
+                <h4>${titulo}</h4>
+                <p><b>${descricao}</b></p>
+              </div>
+            </div>
+        </div>`;
+  conteiner.appendChild(areas);
+}
+
+
+ 
+ 
